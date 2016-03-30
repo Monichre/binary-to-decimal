0f90b07dc7b35b14fb7a7d481781f7ea64dbbb3b
John Klein and Liam Ellis <liam@>
2016-03-30 14:42:20 -0700

    added function to sum even Fibonacci numbers

--- scripts.js
+++ scripts.js
@@ -10,3 +10,23 @@ var binaryToDecimal = function(input, base){
 }
 
 binaryToDecimal('1001001010', 2);
+
+var goldenSpiral = function(num){
+	var num = num - 2;
+  var startArray = [1,2];
+  var evenFib = [];
+  var total = 2;
+  for (var i = 0; i < num; i ++){
+  	startArray.push(startArray[i] + startArray[i+1]);
+    if (startArray[i+2] % 2 === 0) {
+    	total += startArray[i+2];
+    }
+
+  	}
+
+	  console.log(total);
+
+};
+
+
+goldenSpiral(1000000000);
